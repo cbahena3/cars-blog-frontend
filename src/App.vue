91b9270791a1d1b06c797c2d81a99bd4976d8d84
Cristian B <cris.bahena77@gmail.com>
2024-01-24 11:36:56 -0600

    added index

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <script>
+  import axios from "axios";
   export default {
     data: function () {
       return {
-        message: "Welcome to Vue.js!",
+        cars: [],
       };
     },
-    created: function () {},
-    methods: {},
+    created: function () {
+      this.indexCars();
+    },
+    methods: {
+      indexCars: function () {
+        axios.get("/cars.json").then((response) => {
+          console.log("cars index", response);
+          this.cars = response.data;
+        });
+      },
+    },
   };
 </script>
 
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>All Cars</h1>
+    <div v-for="car in cars" v-bind:key="car.id">
+      <h2>{{ car.title }}</h2>
+      <img v-bind:src="car.image" v-bind:alt="car.make" />
+      <p>Description: {{ car.description }}</p>
+      <p>Make: {{ car.make }}</p>
+      <p>Model: {{ car.model }}</p>
+      <p>Color: {{ car.color }}</p>
+      <p>Year: {{ car.year }}</p>
+    </div>
   </div>
 </template>
 
